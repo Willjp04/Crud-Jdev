401a08156c761fba8ac0f3a4d35e3574026925ac
Willian Sostenes <sostenes655@gmail.com>
2022-06-28 07:05:55 -0300

    Método para buscar todos usuários criado e configurado

--- src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,16 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,15 +38,25 @@ public class GreetingsController {
 	@ResponseStatus(code = HttpStatus.OK)
 	@RequestMapping(value = "/{nome}",method = RequestMethod.GET)
 
-	public String retornaOlaMundo(@PathVariable String nome) {
+	public String retornaOlaMundo(@PathVariable String nome, Integer idade) {
 		
 		Usuario usuario = new Usuario();
 		usuario.setNome(nome);
-		
+		usuario.setIdade(15);
 		usuarioRepository.save(usuario);
 				
 		return  nome;
 
+	}
+	
+	@GetMapping("/listartodos")
+	@ResponseBody
+	public ResponseEntity<List<Usuario>> listaUsuarios(){
+		
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+		
 	}
 
 }
