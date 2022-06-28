360f078193e11db964a1e604fd8271ba2b379e0a
Willian Sostenes <sostenes655@gmail.com>
2022-06-28 11:43:36 -0300

    Configurando index para preenchimento dos campos da classe Usuario e botão salvar adicionado

--- src/main/java/br/com/springboot/curso_jdev_treinamento/Application.java
+++ src/main/java/br/com/springboot/curso_jdev_treinamento/Application.java
@@ -7,10 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- *
- * Spring Boot application starter class
- */
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
